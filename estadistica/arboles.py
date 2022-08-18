c345a660619dc95fbf3dfd45bcbed8b714606b35
Agustina Zurletti <agustinazurletti@gmail.com>
2022-08-18 12:40:15 -0300

    Nueva clase Tree con constructor

---
+++ estadistica/arboles.py
@@ -0,0 +1,14 @@
+import random
+
+class Probabilidad:
+    pass
+
+
+class Tree:
+
+    def __init__(self):
+        self.id = random.randint(0, 1000000)
+
+
+    def __str__(self):
+        return f"Tree {self.id}"
